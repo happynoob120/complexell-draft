0bf50c858c57841b0b505af42e409c6e6ca0c7c0
Ekagr Khanna <ekagrkhannaind120@gmail.com>
2026-08-11 09:52:11 +0530

    url change

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import toast from "../utils/toast";
 import ValidationToast from "../components/toasts/ValidationToast";
 
 const api = axios.create({
-  baseURL: "https://localhost:3000/api",
+  baseURL: "https://complexell-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
